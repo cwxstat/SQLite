86c0f8674163991a7a18ab8955c48d9839aef3c9
Mike Chirico <mike.chirico@irhythmtech.com>
2022-11-27 12:32:13 -0500

    Initial Commit

---
+++ SQLite/SQLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteApp.swift
+//  SQLite
+//
+//  Created by Mike Chirico on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
